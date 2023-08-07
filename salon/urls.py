60be804c71856a99b43e44ad9f719582f5b14e91
arturiermolenko <arturiermolenko@gmail.com>
2023-08-07 21:51:20 +0200

    salon/urls.py and salon/views.py filled with empty urls and views

--- salon/urls.py
+++ salon/urls.py
@@ -1,7 +1,41 @@
 from django.urls import path
 
+from salon.views import (
+    index,
+    ProcedureListView,
+    ProcedureCreateView,
+    ProcedureDetailView,
+    ProcedureUpdateView,
+    ProcedureDeleteView,
+    ClientListView,
+    ClientCreateView,
+    ClientDetailView,
+    ClientUpdateView,
+    ClientDeleteView,
+    WorkerListView,
+    WorkerCreateView,
+    WorkerDetailView,
+    WorkerUpdateView,
+    WorkerDeleteView
+)
+
 urlpatterns = [
     path("", index, name="index"),
+    path("procedures/", ProcedureListView.as_view(), name="procedure-list"),
+    path("procedures/create/", ProcedureCreateView.as_view(), name="procedure-create"),
+    path("procedures/<int:pk>/", ProcedureDetailView.as_view(), name="procedure-detail"),
+    path("procedures/<int:pk>/update/", ProcedureUpdateView.as_view(), name="procedure-update"),
+    path("procedures/<int:pk>/delete", ProcedureDeleteView.as_view(), name="procedure-delete"),
+    path("clients/", ClientListView.as_view(), name="client-list"),
+    path("clients/create/", ClientCreateView.as_view(), name="client-create"),
+    path("clients/<int:pk>/", ClientDetailView.as_view(), name="client-detail"),
+    path("clients/<int:pk>/update", ClientUpdateView.as_view(), name="client-update"),
+    path("clients/<int:pk>/delete/", ClientDeleteView.as_view(), name="client-delete"),
+    path("workers", WorkerListView.as_view(), name="worker-list"),
+    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
+    path("workers/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
 ]
 
 app_name = "salon"
--- salon/views.py
+++ salon/views.py
@@ -1,3 +1,67 @@
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    pass
+
+
+class ProcedureListView(LoginRequiredMixin, generic.ListView):
+    pass
+
+
+class ProcedureCreateView(LoginRequiredMixin, generic.CreateView):
+    pass
+
+
+class ProcedureDetailView(LoginRequiredMixin, generic.DetailView):
+    pass
+
+
+class ProcedureUpdateView(LoginRequiredMixin, generic.UpdateView):
+    pass
+
+
+class ProcedureDeleteView(LoginRequiredMixin, generic.DeleteView):
+    pass
+
+
+class ClientListView(LoginRequiredMixin, generic.ListView):
+    pass
+
+
+class ClientCreateView(LoginRequiredMixin, generic.CreateView):
+    pass
+
+
+class ClientDetailView(LoginRequiredMixin, generic.DetailView):
+    pass
+
+
+class ClientUpdateView(LoginRequiredMixin, generic.UpdateView):
+    pass
+
+
+class WorkerListView(LoginRequiredMixin, generic.ListView):
+    pass
+
+
+class WorkerCreateView(LoginRequiredMixin, generic.CreateView):
+    pass
+
+
+class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
+    pass
+
+
+class WorkerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    pass
+
+
+class WorkerDeleteView(LoginRequiredMixin, generic.DeleteView):
+    pass
+
+
+class ClientDeleteView(LoginRequiredMixin, generic.DeleteView):
+    pass
