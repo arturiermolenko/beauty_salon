0eb7fd99acb6f33acbb2d0b9747388fb65218e59
arturiermolenko <arturiermolenko@gmail.com>
2023-08-14 10:09:01 +0200

    Fixed ALLOWED_HOSTS

--- beauty_salon/settings.py
+++ beauty_salon/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "beauty-salon-k4ys.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
